5ec3958a4f87de8466408c9095dee4ac47c23347
jemmalmohamed <jemmalmohamed@gmail.com>
2020-09-22 22:38:01 +0100

    setup passport

--- laravel-admin/database/seeders/UserSeeder.php
+++ laravel-admin/database/seeders/UserSeeder.php
@@ -5,15 +5,17 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        User::factory(20)->create();
+        User::factory()
+            ->times(20)
+            ->create();
     }
 }
