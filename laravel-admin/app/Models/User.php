9ed5a57a62bd860356b46e72a9855be3f36d7969
jemmalmohamed <jemmalmohamed@gmail.com>
2020-09-22 22:49:40 +0100

    setup authentofication

--- laravel-admin/app/Models/User.php
+++ laravel-admin/app/Models/User.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+route::middleware(['auth:api'])->group(function () {
+    Route::get('user', [UserController::class, 'user']);
+    Route::put('users/info', [UserController::class, 'updateInfo']);
+    Route::put('users/password', [UserController::class, 'updatePassword']);
+    Route::apiResource('users', UserController::class);
 });
